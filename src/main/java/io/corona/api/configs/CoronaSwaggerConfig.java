3c50a9cbc00fbecf539bd77599c08d8fd0f6c451
unknown <bunty09.canny09@gmail.com>
2020-06-23 17:21:47 +0530

    string constants commit

--- src/main/java/io/corona/api/configs/CoronaSwaggerConfig.java
+++ src/main/java/io/corona/api/configs/CoronaSwaggerConfig.java
@@ -5,7 +5,6 @@ import static springfox.documentation.builders.PathSelectors.regex;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -17,12 +16,9 @@ public class CoronaSwaggerConfig {
 	public static final String IO_CORONA_API_CONTROLLERS = "io.corona.api.controllers";
 
 	@Bean
-    public Docket productApi() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select().apis(RequestHandlerSelectors.basePackage(IO_CORONA_API_CONTROLLERS))
-                .paths(regex(CORONA_API))
-                .build();
-    }
+	public Docket productApi() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage(IO_CORONA_API_CONTROLLERS)).paths(regex(CORONA_API)).build();
+	}
 
-	
 }
